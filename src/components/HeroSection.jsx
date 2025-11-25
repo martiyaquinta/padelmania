429b61be9bcb6b97c774e1a50de0476776149951
martiyaquinta <estudionomade2025@gmail.com>
2025-11-25 12:16:38 +0100

    style: eliminar indicador de scroll del hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -96,13 +96,6 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-
-      {/* Indicador de scroll */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce-slow">
-        <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-white rounded-full mt-2 animate-pulse"></div>
-        </div>
-      </div>
     </section>
   );
 };
